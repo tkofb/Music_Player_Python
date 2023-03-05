5a2721725935578eab0cd8008818b5900ab85c10
tkofb <76191883+tkofb@users.noreply.github.com>
2023-03-05 18:07:41 -0500

    Added Comments
    Added comments for further explanation of some methods and variables and how python works

--- guiFunctionalityCheatCheat.py
+++ guiFunctionalityCheatCheat.py
@@ -2,13 +2,16 @@ import tkinter as tk
 from tkinter import messagebox
 
 
+# Just how classes work in Python
 class MyGUI:
 
+    # A sort of defautlt constructor in Python
     def __init__(self):
         self.root = tk.Tk()
-        self.menubar = tk.Menu(self.root)
         self.root.title("Music Player")
 
+        # Creation of menus and how they work
+        self.menubar = tk.Menu(self.root)
         self.filemenu = tk.Menu(self.menubar, tearoff=0)
         self.filemenu.add_command(label="Close", command=self.on_closing)
         self.filemenu.add_separator()
@@ -17,11 +20,14 @@ class MyGUI:
         self.actionmenu = tk.Menu(self.menubar, tearoff=0)
         self.actionmenu.add_command(label="Show Message", command=self.show_message)
 
+        # Setting them up in a cascading style
         self.menubar.add_cascade(menu=self.filemenu, label="File")
         self.menubar.add_cascade(menu=self.actionmenu, label="Action")
 
+        # Sets the menu to the new menubar
         self.root.config(menu=self.menubar)
 
+        # Makes it so that the GUI always shows up at the top
         self.root.attributes("-topmost", True)
 
         self.label = tk.Label(self.root, text="Your Message", font=('Arial', 16))
@@ -31,8 +37,8 @@ class MyGUI:
         self.textbox.bind("<KeyPress>", self.shortcut)
         self.textbox.pack(padx=10, pady=10)
 
+        # How checkbuttons are set up and how check state can be used
         self.check_state = tk.IntVar()
-
         self.check = tk.Checkbutton(self.root, text="Show Messagebox", font=('Arial', 16), variable=self.check_state)
         self.check.pack(padx=10, pady=10)
 
@@ -49,10 +55,12 @@ class MyGUI:
 
         self.root.mainloop()
 
+    # Example of static methods in Python through declaration
     @staticmethod
     def static_idea():
         print("Hello World!")
 
+    # Everything past here are just random methods used in the project
     def show_message(self):
         if self.check_state.get() == 0:
             print(self.textbox.get('1.0', tk.END))
